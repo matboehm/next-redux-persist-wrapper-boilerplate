cc631684674ab12ad81055a3f853b1aa755df6f8
Matthias Böhm <mail@matthiasboehm.com>
2023-07-20 10:37:00 +0200

    Change Next project structure

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+// pages/_app.js
+import Head from "next/head";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>Next.js Redux Persist Boilerplate</title>
+            </Head>
+
+            <main id="main">
+                <Component {...pageProps} />
+            </main>
+        </>
+    );
+}
+
+export default MyApp;
